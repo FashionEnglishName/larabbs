b667c1db44aca0e6aa357c934ddbd44536ece3cc
DongMinghui <995862101@qq.com>
2018-03-23 09:36:40 +0000

    topic order

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
@@ -12,5 +13,25 @@ class Topic extends Model
 
     public function user(){
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeWithOrder($query, $order){
+        switch ($order) {
+            case 'recent' :
+                $query = $this->recent();
+                break;
+            default :
+                $query = $this->recentReplied();
+                break;
+        }
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecent($query){
+        return $query->orderBy('created_at', 'desc');
+    }
+
+    public function scopeRecentReplied($query){
+        return $query->orderBy('updated_at', 'desc');
     }
 }
