0dde822eedc74e8e241959ce81ce3c2f30c71f26
DongMinghui <995862101@qq.com>
2018-03-24 05:43:21 +0000

    List topics created by users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -36,7 +36,7 @@ class UsersTableSeeder extends Seeder
 
         $user = User::find(1);
         $user->name = 'dmh';
-        $user->password = 'dddddd';
+        $user->password = bcrypt('dddddd');
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
         $user->email = 'dmh@qq.com';
         $user->save();
